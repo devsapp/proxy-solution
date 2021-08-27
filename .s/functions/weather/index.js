a1fe9f2cceb5d765703c0c9c1a6e413896dcdd31
brickswong@163.com <wb-wll369204@alibaba-inc.com>
2021-08-27 15:09:20 +0800

    feat 获取当前城市天气

--- .s/functions/weather/index.js
+++ .s/functions/weather/index.js
@@ -1,9 +1,37 @@
 const { dk } = require('@serverless-devs/dk');
+var request = require('request');
 
-const baseHandler = (ctx) => {
-  // ctx 包含 ctx.req 可获取入参信息
+const baseHandler =  async(ctx) => {
+  const city = await new Promise((resolve,reject)=>{
+    request('http://www.nmc.cn/rest/position',function(err, httpResponse, body){
+      if(err){
+        reject(err)
+      }else{
+        const {statusCode}= httpResponse
+        resolve({
+          data:JSON.parse(body),
+          statusCode
+        })
+      }
+    })
+  })
+  const weatherUrl = `http://www.nmc.cn/rest/weather?stationid=${city.data.code}`
+  const weather = await new Promise((resolve,reject)=>{
+    request(weatherUrl,function(err, httpResponse, body){
+      if(err){
+        reject(err)
+      }else{
+        const {statusCode}= httpResponse
+        console.log('body',body)
+        resolve({
+          data:JSON.parse(body),
+          statusCode
+        })
+      }
+    })
+  })
   return {
-    json: { title: 'hello serverless dk' },
+    json:weather
   };
 };
 
